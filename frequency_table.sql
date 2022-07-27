9869852874160d5b7007f932ffaa3c9355806d0e
Danil Potylchak <62843834+77n@users.noreply.github.com>
2022-07-27 09:44:03 +0300

    done

--- frequency_table.sql
+++ frequency_table.sql
@@ -1,23 +1,15 @@
--- Як за допомогою SQL-запиту створити гістограму із розміром бінів N?
--- Гистограмму - никак, но можно сделать интервальную таблицу частот
-
 -- MS SQL
+
 DECLARE @N INT
 SET @N = 10
 
-SELECT x.interval, COUNT(x.user_id) as frequency
+SELECT x.start_interval, x.end_interval, COUNT(x.user_id) as frequency
 FROM
-  (SELECT *,
-  CONCAT(
-         CAST(
-            (SELECT MIN(metric_value) FROM table1) + 
-            @N * (FLOOR((metric_value - (SELECT MIN(metric_value) FROM table1)) / @N) + 1) 
-            as varchar), -- начало интервала
-         ' - ',
-         CAST(
-            (SELECT MIN(metric_value) FROM table1) + 
-            @N * (FLOOR((metric_value - (SELECT MIN(metric_value) FROM table1)) / @N) + 2) 
-            as varchar) -- конец интервала == начало следующего интервала
-  ) as interval -- 
-  FROM table_) x
-GROUP BY x.interval
+	(SELECT *,
+		(SELECT MIN(metric_value) FROM table) + @N * (FLOOR((metric_value - (SELECT MIN(metric_value) FROM table)) / @N)) as start_interval,
+		(SELECT MIN(metric_value) FROM table) + @N * (FLOOR((metric_value - (SELECT MIN(metric_value) FROM table)) / @N) + 1) as end_interval
+	FROM table) x
+GROUP BY x.start_interval, x.end_interval
+ORDER BY x.start_interval
+
+-- Будут пропущены интервалы без значений, но это не испортит гистограмму (при отрисовке на числовой оси Х)
